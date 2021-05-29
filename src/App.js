27253654779ac62e4aebba3d56f47ddddd0c960d
Carlos Moreno <gkent@outlook.com>
2021-05-29 09:33:48 -0500

    Primer push

--- src/App.js
+++ src/App.js
@@ -6,17 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hola mundo, los saluda Carlos</h1>
+        <h2><code>(GKentaurus)</code></h2>
+        <h5>Bye...</h5>
       </header>
     </div>
   );
